efad6d1236928e888b055c7e06808dd89d9b1c66
MeghanaVasireddy <mvasireddy@wisc.edu>
2023-01-31 02:19:05 -0600

    started js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var quizQuestions = document.getElementById("quiz-questions");
+var quizSolutions = document.getElementById("quiz-solutions");
+var startQuiz = document.getElementById(".start-quiz");
+var submitQuiz = document.getElementById(".submit-quiz");
+var seconds = 90;
